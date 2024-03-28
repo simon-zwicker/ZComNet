7be95b2ec9c14aa657564dc7d913bfde98f51a37
Simon Zwicker <s.zwicker@appdev.de>
2024-03-28 10:16:50 +0100

    Fixed RequestImage implementation

--- Sources/ZComNet/Utils/Extensions/Data.swift
+++ Sources/ZComNet/Utils/Extensions/Data.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension Data {
+public extension Data {
     func decode<T: Codable>(_ type: T.Type) throws -> T {
         let data = try JSONDecoder().decode(T.self, from: self)
         return data
