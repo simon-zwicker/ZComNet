2e65bee2270cb52e39d8d14d7b9af96021d7f8c6
Simon Zwicker <s.zwicker@appdev.de>
2024-03-28 10:22:08 +0100

    Make RequestImage accessible through package

--- Package.swift
+++ Package.swift
@@ -18,9 +18,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ZComNet"),
+            name: "ZComNet"
+        ),
         .testTarget(
             name: "ZComNetTests",
-            dependencies: ["ZComNet", "RequestImage"]),
+            dependencies: ["ZComNet"]),
     ]
 )
