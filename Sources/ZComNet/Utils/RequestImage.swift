a651346207f78516a3c05c63e6b4845ab80b8d81
Simon Zwicker <s.zwicker@appdev.de>
2024-03-28 10:00:37 +0100

    Fixed RequestImage implementation

--- Sources/ZComNet/Utils/RequestImage.swift
+++ Sources/ZComNet/Utils/RequestImage.swift
@@ -7,12 +7,21 @@
 
 import Foundation
 
+<<<<<<< HEAD
 public class RequestImage {
     public let fileName: String
     public let type: ImageType
     public let data: Data
     public let parameter: String
     public let boundary: String
+=======
+public struct RequestImage {
+    let fileName: String
+    let type: ImageType
+    let data: Data
+    let parameter: String
+    let boundary: String
+>>>>>>> 4404c63 (Fixed RequestImage implementation)
 
     public init(fileName: String, type: ImageType, data: Data, parameter: String, boundary: String) {
         self.fileName = fileName
